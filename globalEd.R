9bfb99094c6cddc21f4ccd3eb71a51187db47cb9
Eliot McIntire <eliotmcintire@gmail.com>
2023-10-20 17:33:55 -0700

    globalEd.R

--- globalEd.R
+++ globalEd.R
@@ -17,6 +17,7 @@ getOrUpdatePkg("SpaDES.project", "0.0.8.9023")
 # devtools::install_github("PredictiveEcology/SpaDES.core", ref = "development") # development #8ddcbe09550c2a901ab446e92c78ebb9859c97c2
 
 ################### RUNAME
+SpaDES.project::pkgload2("~/GitHub/SpaDES.project")
 
 if (SpaDES.project::user("tmichele")) setwd("~/projects/Edehzhie/")
 if (SpaDES.project::user("emcintir")) {
@@ -32,7 +33,7 @@ if (SpaDES.project::user("emcintir")) {
 # sppEquiv <- sppEquiv_CA(runName)
 ################ SPADES CALL
 library(SpaDES.project)
-pkgload::load_all("c:/Eliot/GitHub/SpaDES.project")
+# pkgload::load_all("c:/Eliot/GitHub/SpaDES.project")
 out <- SpaDES.project::setupProject(
   runName = "Edehzhie",
   Restart = TRUE,
@@ -40,7 +41,8 @@ out <- SpaDES.project::setupProject(
                scratchPath = "~/scratch"),
   modules =
     file.path("PredictiveEcology",
-              c(paste0(# terra-migration
+              c("canClimateData@canadianProvs",
+                paste0(# terra-migration
                 c("Biomass_speciesData",
                   "Biomass_core"),
                 "@terra-migration"),
@@ -48,7 +50,6 @@ out <- SpaDES.project::setupProject(
                   c("Biomass_borealDataPrep",
                     "Biomass_speciesFactorial",
                     "Biomass_speciesParameters",
-                    "canClimateData",
                     "fireSense_dataPrepFit",
                     "fireSense_IgnitionFit",
                     "fireSense_EscapeFit",
@@ -62,6 +63,7 @@ out <- SpaDES.project::setupProject(
   functions = "tati-micheletti/Edehzhie@master/inputs/outterFuns.R",
   options = list(spades.allowInitDuringSimInit = TRUE,
                  reproducible.showSimilar = TRUE,
+                 reproducible.memoisePersist = TRUE,
                  reproducible.inputPaths = "~/data",
                  reproducible.showSimilarDepth = 5,
                  gargle_oauth_email = if (user("tmichele")) "tati.micheletti@gmail.com" else NULL,
@@ -70,39 +72,42 @@ out <- SpaDES.project::setupProject(
   ),
   times = list(start = 2011,
                end = 2025),
+  params = list(.globals = list(.plots = NA, .plotInitialTime = NA,
+                                sppEquivCol = 'Boreal')),
   require = "PredictiveEcology/reproducible@reproducibleTempCacheDir (>= 2.0.8.9010)", # so can use Cache next
-  studyArea = Cache(studyAreaGenerator()),
-  rasterToMatch = Cache(rtmGenerator(sA = studyArea)),
-  studyAreaLarge = Cache(studyAreaGenerator(large = TRUE, destPath = paths[["inputPath"]])),
-  rasterToMatchLarge = Cache(rtmGenerator(sA = studyAreaLarge, destPath = paths[["inputPath"]])),
-  sppEquiv = sppEquiv_CA(runName),
-  params = list(.globals = list(sppEquivCol = runName,
-                                dataYear = "2011",
-                                .useCache = c(".inputObjects", "init"),
-                                .studyAreaName = "NT"),
-                fireSense_IgnitionFit = list(lb = list(coef = 0,
-                                                       #I got this from running only dataPrepFit, then quantile(MDC, probs = 0.05) NOTE: Ian's notes
-                                                       knots = list(MDC = 100)),
-                                             ub = list(coef = 20,
-                                                       #and the upper quantile was 0.9 NOTE: Ian's notes
-                                                       knots = list(MDC = 156))),
-                canClimateData = list(.runName = runName,
-                                      .useCache = ".inputObjects",
-                                      climateGCM = "CanESM5",
-                                      climateSSP = "370",
-                                      historicalFireYears = 1991:2022,
-                                      studyAreaName = "NT")
-  ),
+  # studyArea = Cache(studyAreaGenerator()),
+  # rasterToMatch = Cache(rtmGenerator(sA = studyArea)),
+  # studyAreaLarge = Cache(studyAreaGenerator(large = TRUE, destPath = paths[["inputPath"]])),
+  # rasterToMatchLarge = Cache(rtmGenerator(sA = studyAreaLarge, destPath = paths[["inputPath"]])),
+  # sppEquiv = sppEquiv_CA(runName),
+  # params = list(.globals = list(sppEquivCol = runName,
+  #                               dataYear = "2011",
+  #                               .plotInitialTime = NA,
+  #                               .useCache = c(".inputObjects", "init")),
+  #                               #.studyAreaName = "NT"),
+  #               fireSense_IgnitionFit = list(lb = list(coef = 0,
+  #                                                      #I got this from running only dataPrepFit, then quantile(MDC, probs = 0.05) NOTE: Ian's notes
+  #                                                      knots = list(MDC = 100)),
+  #                                            ub = list(coef = 20,
+  #                                                      #and the upper quantile was 0.9 NOTE: Ian's notes
+  #                                                      knots = list(MDC = 156))),
+  #               canClimateData = list(.runName = runName,
+  #                                     .useCache = ".inputObjects",
+  #                                     climateGCM = "CanESM5",
+  #                                     climateSSP = "370",
+  #                                     historicalFireYears = 1991:2020)#,
+  #                                     #studyAreaName = "NT")
+  # ),
+  studyArea = list(level = 2, NAME_2 = "Fort Smith"),
+  studyAreaLarge = studyArea,
   packages = c("googledrive", 'RCurl', 'XML',
                "PredictiveEcology/LandR@development (>= 1.1.0.9073",
                "PredictiveEcology/SpaDES.core@optionsAsArgs (>= 2.0.2.9009)"),
   useGit = "sub"
 )
 
-#pkgload::load_all("c:/Eliot/GitHub/SpaDES.project")
-pkgload::load_all("c:/Eliot/GitHub/reproducible")
-pkgload::load_all("c:/Eliot/GitHub/SpaDES.core")
-pkgload::load_all("c:/Eliot/GitHub/LandR")
-
+SpaDES.project::pkgload2(
+  list(file.path("~/GitHub", c("reproducible", "SpaDES.core", "LandR")),
+       "~/GitHub/SpaDES.project"))
 
 snippsim <- do.call(SpaDES.core::simInitAndSpades, out)
